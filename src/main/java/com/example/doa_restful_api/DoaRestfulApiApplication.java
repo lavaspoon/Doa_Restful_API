49f97cf67199f516dc3a590bf4812be3f9f060db
lavaspoon <lavaspoon@naver.com>
2021-07-18 00:43:22 +0900

    1. Project Create

---
+++ src/main/java/com/example/doa_restful_api/DoaRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.doa_restful_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoaRestfulApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoaRestfulApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/doa_restful_api/DoaRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doa_restful_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoaRestfulApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
