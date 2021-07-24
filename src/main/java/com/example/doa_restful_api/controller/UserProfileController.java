b85500bd7fb980678802d795b9383313ba5b16fc
lavaspoon <lavaspoon@naver.com>
2021-07-24 23:12:58 +0900

    test commit and pust

--- src/main/java/com/example/doa_restful_api/controller/UserProfileController.java
+++ src/main/java/com/example/doa_restful_api/controller/UserProfileController.java
@@ -30,7 +30,7 @@ public class UserProfileController {
     public List<UserProfile> getUserProfileList() {
         return new ArrayList<UserProfile>(userMap.values());
     }
-
+    //값 insert  
     @PutMapping("/user/{id}")
     public void putUserProfile(@PathVariable("id") String id, @RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("address") String address) {
         UserProfile userProfile = new UserProfile(id, name, phone, address);
