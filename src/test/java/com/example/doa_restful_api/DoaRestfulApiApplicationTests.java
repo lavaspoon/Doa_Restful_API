f122bb44436448c1fc35b5d8c39efcb3989a2b46
lavaspoon <lavaspoon@naver.com>
2021-08-15 00:00:30 +0900

    db navi set

--- src/test/java/com/example/doa_restful_api/DoaRestfulApiApplicationTests.java
+++ src/test/java/com/example/doa_restful_api/DoaRestfulApiApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.doa_restful_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-//db test
+//db test1234
 @SpringBootTest
 class DoaRestfulApiApplicationTests {
 
